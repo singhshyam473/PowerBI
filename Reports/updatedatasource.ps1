a987420d8733c3b648636871126af239bb56dbbc
Shyam Singh <singhshyam473@gmail.com>
2020-05-28 17:54:42 +0530

    test files

---
+++ Reports/updatedatasource.ps1
@@ -0,0 +1,41 @@
+﻿#update the Datasource Credentials
+$datasetname = "name"
+$workspacename = $env:dev_workspacename
+
+$clientsec = "$(client_secret)" | ConvertTo-SecureString -AsPlainText -Force
+
+$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $env:client_id, $clientsec
+Connect-PowerBIServiceAccount -ServicePrincipal -Credential $credential -TenantID $env:tenant_id
+
+#GetWorkspace
+
+$workspace = Get-PowerBIWorkspace -Name $workspacename
+
+#GetDataSets
+$DatasetRespone = Invoke-PowerBIRestMethod -Url "groups/$($workspace.id)/datasets" -Method Get | ConvertFrom-Json
+
+#Get Dataset
+$datasets = $DatasetRespone.value
+
+foreach($dataset in $datasets){
+    if($dataset.name -eq $datasetname){
+        $datasetid = $dataset.id;
+        break;
+    }
+}
+
+#Takeover Dataset
+Invoke-PowerBIRestMethod -Url "groups/$($workspace.id)/datasets/$($datasetid)/Default.TakeOver" -Method POST
+
+#update Datasource credentials
+$BounGateway = Invoke-PowerBIRestMethod -Url "groups/$($workspace.id)/datasets/$($datasetid)/Default.GetBoundGatewayDataSources" -Method GET | ConvertFrom-Json
+
+$UpdateUserCredential = @{
+        credentialType = "Basic"
+        basicCredentials = @{
+        username = $($env:sqlusername)
+        password = "$(credentialPassword)"
+        }
+} | ConvertTo-Json
+
+Invoke-PowerBIRestMethod -Url "gateways/$($BounGateway.value.gatewayId)/datasources/$($BounGateway.value.id)" -Method PATCH -Body $UpdateUserCredential | ConvertFrom-Json
---
+++ Reports/updateparameters.ps1
@@ -0,0 +1,46 @@
+﻿$datasetname = "name"
+$workspacename = $env:qa_workspacename
+
+try{
+
+$clientsec = "$(client_secret)" | ConvertTo-SecureString -AsPlainText -Force
+
+$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $env:client_id, $clientsec
+Connect-PowerBIServiceAccount -ServicePrincipal -Credential $credential -TenantID $env:tenant_id
+
+$workspace = Get-PowerBIWorkspace -Name $workspacename
+
+$DatasetRespone = Invoke-PowerBIRestMethod -Url "groups/$($workspace.id)/datasets" -Method Get | ConvertFrom-Json
+
+$datasets = $DatasetRespone.value
+
+foreach($dataset in $datasets){
+    if($dataset.name -eq $datasetname){
+        $datasetid = $dataset.id;
+        break;
+    }
+}
+
+$postParams = @{
+    updateDetails = @(
+    @{
+        name = "DataBase"
+        newValue = "$($env:qa_database)"
+    }
+    )
+} | ConvertTo-Json
+
+Invoke-PowerBIRestMethod -Url "groups/$($workspace.id)/datasets/$($datasetid)/Default.TakeOver" -Method POST
+
+$postParams
+Write-Host "dataset: " $datasetid
+Write-Host "workspace: " $workspace.id
+
+
+$response = Invoke-PowerBIRestMethod -Url "groups/$($workspace.id)/datasets/$($datasetid)/Default.UpdateParameters" -Method Post -Body $postParams | ConvertFrom-Json
+
+}
+
+catch{
+Resolve -PowerBIError
+}
